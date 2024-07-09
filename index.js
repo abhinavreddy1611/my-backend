9a8c741c96bc2bc58e4cd27ad121f779155cbfb1
abhinavreddy1611 <141564653+abhinavreddy1611@users.noreply.github.com>
2024-07-09 19:47:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app=express();
 require('dotenv').config();
 const mongoose=require("mongoose");
 const router=require("./Routes/faculty.js");
+const cors=require("cors");
+app.use("cors");
 app.use(express.json());
 app.use("/",router);
+
 
 
 
